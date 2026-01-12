cb2ce096fd3d98d190699074fb574391ce4db640
aisyah3000 <aisyahbgr03@gmail.com>
2026-01-12 22:59:47 +0700

    update project mutabaah

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
+require('dotenv').config(); // Pastikan ini ada agar bisa baca file .env
 const mongoose = require('mongoose');
 
-// Gunakan nama 'projectmutabaah' tepat sebelum tanda tanya (?)
-const mongoURI = "mongodb+srv://aisyahnasution3300_db_user:proyek4matkul@proyekita.pwydp0t.mongodb.net/projectmutabaah?retryWrites=true&w=majority";
+// Ambil URL dari .env
+const mongoURI = process.env.MONGO_URI; 
 
 const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI);
-        // Tambahkan baris ini untuk memverifikasi nama database di terminal
         console.log(`✅ Terhubung ke Database: ${mongoose.connection.name}`);
     } catch (err) {
         console.error("❌ Gagal terhubung:", err.message);
